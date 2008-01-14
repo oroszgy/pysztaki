70929d16f05f431327c1f1954f0a12ac3b6a3a13
Dániel Pék <pekdaniel@gmail.com>
2008-01-14 14:00:23 +0000

    Initial commit

---
+++ pysztaki.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""
+PySztaki
+Konzolos fordito-script a sztaki adatbazis hasznalataval
+
+Egyszeru script, mely a parameterkent kapott szot megkeresi
+a szinten parameterkent kapott sztaki szotar-valtozatban a
+szotar.sztaki.hu-n, majd BeautifulSouppal benyalja az oldalt,
+es kiszedi a talalatokat. A script translate() fuggvenye vegzi
+a tenyleges forditast, konnyen felhasznalhato mas projectben is.
+
+Tovabbi informacio a project honlapjan:
+http://code.google.com/p/pysztaki/
+
+Copyright (c) 2008, Pek Daniel
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the <ORGANIZATION> nor the names of its
+      contributors may be used to endorse or promote products derived from
+      this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+import sztakiconfig
+import sys
+import urllib
+import utils
+
+def print_results(results):
+    import locale
+    
+    encoding = locale.getpreferredencoding(True)
+    for word in results.keys():
+        line = (word + u": " + u"; ".join(results[word])).encode(encoding)
+        print line
+
+def translate(form_data):
+    """
+    Ez a fuggveny vegzi a kapott form-adatok alapjan a lekerdezest.
+    A visszaadott ertek egy multidict, ami tartalmazza az egyes szavak
+    jelenteseit, mint egyszeru list objektumokat.
+    """
+    
+    # parameterek rekombinalasa, eredmeny letoltese
+    params = urllib.urlencode(utils.encode_dict(form_data, "iso8859-2"))
+    page = urllib.urlopen(sztakiconfig.base_url + "?%s" % params)
+    
+    # az eredmenyt benyaljuk beautifulsouppal
+    # hasznalunk egy util-fuggvenyt, ami lenyeli a whitespace-eket
+    soup = utils.bs_preprocess(page)
+    
+    # adatok kinyerese
+    result = utils.odict()
+    current_word = ""
+    
+    # eredmenytablak megkeresese, vegigiteralunk rajtuk
+    result_tables = soup.findAll(name = "table", attrs = { 'class': 'results' })
+    for result_table in result_tables:
+        # megkeressuk az osszes sort, vegigmegyunk rajtuk
+        result_rows = result_table.findAll(name = "tr")
+        for result_row in result_rows:
+            # ha mainrow, akkor letrehozunk egy uj dict-keyt neki, es betesszuk
+            # az elso jelentest a tombjebe
+            if result_row['id'][:4] == "main":
+                tds = result_row.findAll(name = "td")
+                current_word = utils.strip_tags(tds[0])
+                if current_word == "":
+                    continue;
+                meaning = utils.strip_tags(tds[1])
+                if meaning == "":
+                    continue;
+                if result.has_key(current_word):
+                    result[current_word].append(meaning)
+                else:
+                    result[current_word] = [meaning]
+            # egyebkent csak hozzaadjuk az aktualis szo jelenteseihez
+            else:
+                td = result_row.find(name = "td")
+                meaning = utils.strip_tags(td)
+                if meaning == "":
+                    continue;
+                result[current_word].append(meaning)
+    
+    return result
+
+if __name__ == '__main__':
+    # parameterek parseolasa, config-fajl feldolgozasa
+    import locale
+    
+    encoding = locale.getpreferredencoding(True)
+    if len(sys.argv) < 2:
+        sys.stderr.write(u"Helytelen paraméterezés!\n".encode(encoding))
+    
+    # az elso parameter alapesetben a nyelv, a tobbi a szo
+    args = utils.decode_list(sys.argv, encoding)
+    dict_id = args[1]
+    word = u" ".join(args[2:])
+
+    # form alapertekek kinyerese a config-fajlbol
+    form_data = sztakiconfig.form_data
+    
+    # ha letezik a megadott nyelv-azonosito, akkor hasznaljuk,
+    # ha nem, akkor default ertekkel dolgozunk, es hozzafuzzuk
+    # az elso parametert is a forditando szohoz
+    if sztakiconfig.dict_ids.has_key(dict_id):
+        form_data['L'] = sztakiconfig.dict_ids[dict_id]
+    else:
+        word = " ".join([dict_id, word])
+    
+    form_data[sztakiconfig.word_param] = word
+    
+    # translate fuggveny meghivasa
+    result = translate(form_data)
+    
+    print_results(result)
+    
---
+++ sztakiconfig.py
@@ -0,0 +1,50 @@
+# -*- encoding: utf-8 -*-
+
+# A sztaki kereso-urlje
+base_url = u"http://szotar.sztaki.hu/dict_search.php"
+
+# ismeretlen celu form-parameterek
+form_data = {
+    u'flash':   u"",
+    u'E':       u"1",
+    u'in_form': u"1",
+
+# ismert form-parameterek
+    u'L':      u"ENG:HUN:EngHunDict",   # nyelv
+    u'M':      u"1",                    # egyezes
+                                        # 2 - barmilyen
+                                        # 1 - szoeleji
+                                        # 0 - csak teljes szavak
+                                        # 3 - teljes egyezes
+    u'C':      u"0",                    # kis-nagybetu nem szamit
+    u'A':      u"0",                    # ekezet nem szamit 
+}
+
+
+
+# A tovabbi beallitasokat csak akkor modositsd, ha jol tudod, mit csinalsz
+# megfeleltetesek a sztaki-s es a parancssori jelolesek kozott
+dict_ids = {
+    u'enhu':    u"ENG:HUN:EngHunDict",
+    u'huen':    u"HUN:ENG:EngHunDict",
+    u'gehu':    u"GER:HUN:GerHunDict",
+    u'huge':    u"HUN:GER:GerHunDict",
+    u'frhu':    u"FRA:HUN:FraHunDict",
+    u'hufr':    u"HUN:FRA:FraHunDict",
+    u'ithu':    u"ITA:HUN:ItaHunDict",
+    u'huit':    u"HUN:ITA:ItaHunDict",
+    u'hohu':    u"HOL:HUN:HolHunDict",
+    u'huho':    u"HUN:HOL:HolHunDict",
+    u'pohu':    u"POL:HUN:PolHunDict",
+    u'hupo':    u"HUN:POL:PolHunDict",
+}
+
+# Sztaki form-valtozok
+word_param = u"W"
+
+# Sztaki parameterek, amelyek megvaltoztatasa esetleg a python kod valtoztatasat igenylik
+form_data.update({
+    u'P':               u"0",    # kiejtes nelkul
+    u'T':               u"1",    # tablazatos megjelenites
+    u'hallatlan_inc':   u"0",    # jelbeszed megjelenitese
+})
